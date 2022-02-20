b25dd04d7bb2af9297909957d583d295cbe6689d
marvinscheffold <m@marvinscheffold.com>
2022-02-20 19:51:37 +0000

    counter ready

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,10 +4,22 @@ import { useState } from "react";
 export function Counter() {
     const [count, setCount] = useState(0);
     return (
-        <div className="counter">
-            <button onClick={() => setCount(count - 1)}>-</button>
-            <div className="text-3xl font-bold underline">{count}</div>
-            <button onClick={() => setCount(count + 1)}>+</button>
+        <div className="flex text-white text-6xl p-4 font-bold">
+            <button
+                className="flex items-center justify-center bg-blue px-6 pb-2 w-24 h-24 font-bold"
+                onClick={() => setCount(count - 1)}
+            >
+                -
+            </button>
+            <div className="w-28 h-24 p-4 text-center flex items-center justify-center bg-secondary-dark-400">
+                {count}
+            </div>
+            <button
+                className="flex items-center justify-center bg-blue px-6 w-24 h-24 font-bold"
+                onClick={() => setCount(count + 1)}
+            >
+                +
+            </button>
         </div>
     );
 }

f8fd2d2b648cb0c190e7417b09e6a39dcc03958c
marvinscheffold <m@marvinscheffold.com>
2022-02-21 01:45:51 +0000

    after video

--- src/app.jsx
+++ src/app.jsx
@@ -6,7 +6,7 @@ import { LoginPage } from "./components/login-page";
 export default function App() {
     return (
         <div className="h-screen bg-secondary">
-            <Counter />
+            <LoginPage />
         </div>
     );
 }
--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,14 +1,32 @@
 import React from "react";
-import { useState } from "react";
+import { useReducer } from "react";
+
+const reducerFunction = (state, action) => {
+    switch (action.type) {
+        case "increase":
+            return { ...state, count: state.count + 1 };
+        case "decrease":
+            return { ...state, count: state.count - 1 };
+        default:
+            return state;
+    }
+};
+
+const initialState = {
+    count: 0,
+};
 
 export function Counter() {
-    const [count, setCount] = useState(0);
+    const [state, dispatch] = useReducer(reducerFunction, initialState);
+
+    const { count } = state;
+
     return (
         <div className="h-full flex items-center justify-center">
             <div className="flex text-white text-6xl p-4 font-bold">
                 <button
                     className="flex items-center justify-center bg-blue px-6 pb-2 w-24 h-24 font-bold"
-                    onClick={() => setCount((count) => count - 1)}
+                    onClick={() => dispatch({ type: "decrease" })}
                 >
                     -
                 </button>
@@ -17,7 +35,7 @@ export function Counter() {
                 </div>
                 <button
                     className="flex items-center justify-center bg-blue px-6 w-24 h-24 font-bold"
-                    onClick={() => setCount((count) => count + 1)}
+                    onClick={() => dispatch({ type: "increase" })}
                 >
                     +
                 </button>
